dfe40d2a5f485a86ef3fb21658331aa203171a04
Danylo Kucherenko <danylo.kucherenko@gmail.com>
2022-05-03 20:21:09 +0100

    ApplyingOCP to RatingEngine

---
+++ ArdalisRating/RaterFactory.cs
@@ -0,0 +1,18 @@
+namespace ArdalisRating;
+
+public class RaterFactory
+{
+    public Rater Create(Policy policy, RatingEngine engine)
+    {
+        try
+        {
+            return (Rater) Activator.CreateInstance(
+                Type.GetType($"ArdalisRating.{policy.Type}PolicyRater"),
+                new object[] {engine, engine.Logger});
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
